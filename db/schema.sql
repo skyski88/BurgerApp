acdf64e10b891692623ca02a17a933bad81567ee
Bradley Williams <skyski88@gmail.com>
2020-11-22 21:00:12 -0500

    commit

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS burgers_db;
+
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+id INT AUTO_INCREMENT PRIMARY KEY,
+burger_name VARCHAR(225) NOT NULL,
+devoured BOOL DEFAULT false,
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Guacamole Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chili Frito Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried Egg Burger', FALSE);
